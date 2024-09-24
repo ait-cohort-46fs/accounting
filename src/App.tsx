7e8a6f44fd847890bac043dc1e67db2fb1a87b22
edward <edwardlfish@gmail.com>
2024-09-24 10:57:47 +0300

    added routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
+import { Navigate, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Guest from './components/Guest'
 import Profile from './components/Profile'
 
 function App() {
-
+  // TODO get token from redux state
+  const token = 'gkfh';
 
   return (
-    <Profile />
+    <Routes>
+      <Route path='/' element={token ? <Navigate to='/profile' /> : <Guest />} />
+      <Route path='/profile' element={token ? <Profile /> : <Navigate to='/' />} />
+    </Routes>
   )
 }
 
