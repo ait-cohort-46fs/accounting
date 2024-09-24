84e005339e8f4cd5469dee00e9e2591e2914f431
edward <edwardlfish@gmail.com>
2024-09-24 13:54:52 +0300

    localstorage with state

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,12 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
 import token from "../features/slices/tokenSlice";
 import user from "../features/slices/userSlice";
+import { UserProfile } from "../utils/type";
+
+const preloadedState = JSON.parse(localStorage.getItem('state') ?? '{}') as {user: UserProfile, token: string};
 
 export const store = configureStore({
     reducer: {
         user, token
-    }
+    },
+    preloadedState
 })
+
+store.subscribe(() => localStorage.setItem('state', JSON.stringify(store.getState())));
 
 // Infer the `RootState`,  `AppDispatch`, and `AppStore` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
